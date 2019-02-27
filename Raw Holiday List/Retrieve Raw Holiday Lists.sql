4840b6833e7fec95560051dde8be7cd45f563a27
my30 <myang30@outlook.com>
2019-02-27 15:49:26 +0000

    Original SQL extraction scripts added.

---
+++ Raw Holiday List/Retrieve Raw Holiday Lists.sql
@@ -0,0 +1,19 @@
+SELECT dhu.id, dhu.start_date, dhu.end_date, dhu.duration
+FROM dim_holidays_uk dhu
+WHERE dhu.country = 'Scotland'
+GROUP BY dhu.id;
+
+SELECT dhu.id, dhu.start_date, dhu.end_date
+FROM dim_holidays_uk dhu
+WHERE dhu.country = 'Wales'
+GROUP BY dhu.id;
+
+SELECT dhu.id, dhu.start_date, dhu.end_date
+FROM dim_holidays_uk dhu
+where dhu.country = 'Northern Ireland'
+GROUP BY dhu.id;
+
+SELECT dhu.id, dhu.start_date, dhu.end_date
+FROM dim_holidays_uk dhu
+WHERE dhu.country = 'England'
+GROUP BY dhu.id;
