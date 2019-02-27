3e3021061271cbb294bb74fc2b253d20854baa8d
my30 <myang30@outlook.com>
2019-02-27 14:33:09 +0000

    Code cleaned.

--- Scotland Holiday Dates Calendar.py
+++ Scotland Holiday Dates Calendar.py
@@ -1,28 +1,27 @@
 # Ming 26/Feb/2019
 import pandas as pd
 
+# Read the holiday list into a DataFrame
 df_scotland_holidays = pd.read_csv('Raw Holiday List/Scotland dim_holidays_uk.csv', sep=',',
                                    names=['id', 'start_date', 'end_date', 'duration'],
                                    parse_dates=['start_date', 'end_date'])
-
+# Truncate the final .csv file
 file = open('Exported Calendars/holiday_calendar_scotland.csv', mode='w+')
 file.close()
 print('holiday_calendar_scotland.csv is truncated!')
 
 for i in range(0, df_scotland_holidays.shape[0]):
-    # Sample holiday: line 1 -> line i
     df_sample_holiday = df_scotland_holidays.iloc[i, ]
-    # print(df_sample_holiday)
     # Access that row with iloc/loc and specify the column index/column to get int64 or float data.
-    holiday_duration = df_sample_holiday[3]
+    # Retrieve the value from series.
     holiday_start = df_sample_holiday[1]
     holiday_end = df_sample_holiday[2]
-    # Retrieve the value from series.
+    # Convert TimeSeries to strings
     holiday_start = str(holiday_start)
     holiday_end = str(holiday_end)
-    # print('\n', holiday_start, '\n', type(holiday_end), '\n')
+    # print('\n', type(holiday_end), '\n')
+    # Produce calendar
     date_range = pd.date_range(start=holiday_start, end=holiday_end, freq='D')
-    # print(date_range, '\n')
     # Now generate a new DataFrame containing the data wanted.
     df_holiday_dates = pd.DataFrame(date_range, columns=['date'])
     # With uids for holidays
